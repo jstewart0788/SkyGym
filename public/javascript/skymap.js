e62ccba206c6da9b2cc8e25b3626827f3bfd5331
Jonathan Stewart <jstewart0788@gmail.com>
2016-09-13 20:26:57 -0400

    CSS and HTTP error changes

--- public/javascript/skymap.js
+++ public/javascript/skymap.js
@@ -6,6 +6,6 @@ var wwt;
  }
 
  function wwtReady() {
-     wwtControl.loadImageCollection("http://www.worldwidetelescope.org/COMPLETE/wwtcomplete.wtml");
+     wwtControl.loadImageCollection("https://www.worldwidetelescope.org/COMPLETE/wwtcomplete.wtml");
      // Put your custom initialization code here.
  }
