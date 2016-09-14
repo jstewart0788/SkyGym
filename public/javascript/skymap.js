762b5bc5489275bd4c807592f62513c8b79715ea
Jonathan Stewart <jstewart0788@gmail.com>
2016-09-13 20:30:29 -0400

    test

--- public/javascript/skymap.js
+++ public/javascript/skymap.js
@@ -6,6 +6,6 @@ var wwt;
  }
 
  function wwtReady() {
-     wwtControl.loadImageCollection("https://www.worldwidetelescope.org/COMPLETE/wwtcomplete.wtml");
+     wwtControl.loadImageCollection("http://www.worldwidetelescope.org/COMPLETE/wwtcomplete.wtml");
      // Put your custom initialization code here.
  }
